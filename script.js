a44c2c67fd4a6b817acaa71f53db179befa2bab8
fer <fernandosotoibarra96@gmail.com>
2024-08-27 15:03:53 -0600

    agregando detalles y boton copiar

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const textarea = document.querySelector(".m_textarea");
 const mensaje = document.querySelector(".mensaje");
+document.querySelector(".btmCopiar").style.visibility = 'hidden'
+
 /*La letra "e" es convertida para "enter"
 La letra "i" es convertida para "imes"
 La letra "a" es convertida para "ai"
@@ -11,11 +13,13 @@ function btnEncriptar(){
     mensaje.value = textoEncriptado;
     textarea.value = "";
     mensaje.style.backgroundImage = "none";
+    document.querySelector('.btmCopiar').style.visibility = 'visible';
 }
 function btnDesencriptar(){
     const textoDesencriptado = Desencriptar(textarea.value);
     mensaje.value = textoDesencriptado;
     textarea.value = "";
+    document.querySelector('.btmCopiar').style.visibility = 'visible';
 }
 function Encriptar(stringEncriptado){
     let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
@@ -39,3 +43,18 @@ function Desencriptar(stringDesencriptado){
     return stringDesencriptado;
 }
 
+function copiarTexto() {
+    const texto = document.querySelector(".mensaje").value;
+
+    if (navigator.clipboard) {
+        navigator.clipboard.writeText(texto)
+            .then(() => {
+                alert("Texto copiado al portapapeles");
+            })
+            .catch(err => {
+                alert("Error al copiar el texto: ", err);
+            });
+    } else {
+        alert("La API del portapapeles no está disponible en este navegador.");
+    }
+}
